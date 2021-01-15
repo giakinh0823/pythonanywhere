a979f766e1620de735c6557e18bcab734a4057e2
Gia Kinh <giakinh2000@gmail.com>
2021-01-15 15:40:54 +0700

    done

--- saleProduct/settings.py
+++ saleProduct/settings.py
@@ -159,7 +159,5 @@ EMAIL_HOST_PASSWORD = '123@123a'
 
 
 import dj_database_url 
-import django_heroku
-django_heroku.settings(locals())
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
