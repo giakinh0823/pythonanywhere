678531484d618bbe77304b9d162a5407b4f6b24f
Gia Kinh <giakinh2000@gmail.com>
2021-01-15 15:21:46 +0700

    okokok

--- saleProduct/settings.py
+++ saleProduct/settings.py
@@ -137,8 +137,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
