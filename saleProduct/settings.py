e6b0aca743f4e3d32b3e52636787e1dc1bdd4bc5
Gia Kinh <giakinh2000@gmail.com>
2021-01-15 15:39:27 +0700

    ok

--- saleProduct/settings.py
+++ saleProduct/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
-
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -63,7 +61,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 
 ROOT_URLCONF = 'saleProduct.urls'
 
@@ -162,6 +159,7 @@ EMAIL_HOST_PASSWORD = '123@123a'
 
 
 import dj_database_url 
+import django_heroku
 django_heroku.settings(locals())
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
